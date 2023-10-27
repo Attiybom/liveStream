b2abc20ff35819e7dd83058d494db9b49261d549
luminous <910667665@qq.com>
2023-10-28 02:23:34 +0800

    feat: 环境配置+部分首页搭建（todo）

---
+++ pages/my/my.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="flex">
+		<view class="bg-danger flex-1">my</view>
+		<view class="bg-main flex-1">blue</view>
+		<text class="iconfont icon-gengduo" style="font-size: 100rpx;"></text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hisasd, liveStream!!!'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
